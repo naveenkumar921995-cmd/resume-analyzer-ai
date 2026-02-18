d666363a9b305083c555e1992c95fa37b5d06a71
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-18 07:44:23 +0530

    Create similarity.py

---
+++ utills/similarity.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def calculate_similarity(resume_text, jd_text):
+    embeddings = model.encode([resume_text, jd_text])
+    similarity_score = cosine_similarity(
+        [embeddings[0]], [embeddings[1]]
+    )[0][0]
+
+    return round(similarity_score * 100, 2)
