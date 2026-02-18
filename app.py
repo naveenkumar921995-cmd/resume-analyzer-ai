c97af66fe23fd79b59f0806b0ae7b3ba55d035be
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-18 20:15:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import sys
 import os
-import streamlit as st
 
-# Add project root to Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from utils.pdf_parser import extract_text_from_pdf
