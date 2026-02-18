d771dccce97779e280c08617b2ccf4e8f006b1c3
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-18 07:52:13 +0530

    Update __init__.py

--- utills/__init__.py
+++ utills/__init__.py
@@ -1,11 +1,1 @@
-resume-analyzer-ai/
-│
-├── app.py
-├── utils/
-│   ├── __init__.py   👈 ADD THIS
-│   ├── pdf_parser.py
-│   ├── text_cleaner.py
-│   ├── skill_extractor.py
-│   ├── similarity.py
-│   ├── ats_score.py
-│   ├── llm_suggestions.py
+
