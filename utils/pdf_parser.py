2b41bc75538d0b8938848c982c4dd0f82640ce06
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-02-18 20:22:54 +0530

    Update pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,1 +1,11 @@
+import PyPDF2
+
+def extract_text_from_pdf(uploaded_file):
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
+    text = ""
+
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    return text
 
